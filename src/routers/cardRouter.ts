5908071292c5f180f8923a52a83c7e50fc3b211f
ErickSchuenck <esfmokdeci@gmail.com>
2022-07-18 00:10:29 -0300

    função criar cartão criada

--- src/routers/cardRouter.ts
+++ src/routers/cardRouter.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { cardSchema } from "../schemas/schemas.js";
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { validateToken } from "../middlewares/validateToken.js";
-import { registerCard } from "../controllers/cardsController.js";
+import { registerCard, getAllCards } from "../controllers/cardsController.js";
 
 
 const cardRouter = Router();
 cardRouter.post("/card", validateToken, validateSchema(cardSchema), registerCard);
+cardRouter.get("/card", validateToken, getAllCards);
+
 
 export default cardRouter;
