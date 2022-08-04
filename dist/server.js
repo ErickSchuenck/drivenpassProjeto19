d9127ecb42864038391111c04d29f673f647d1db
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-04 14:54:38 -0300

    Preparando revisão de código

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js"
-import dotenv from "dotenv";
-dotenv.config();
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server up and running on port ${port}`);
-});
